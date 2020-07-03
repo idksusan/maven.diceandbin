bd36841695ab651461b15cc37c779082a3254725
Susan Tabassum <sttabassum93@gmail.com>
2020-07-02 22:59:00 -0400

    Working on Bin class

--- src/main/java/com/github/curriculeon/Bin.java
+++ src/main/java/com/github/curriculeon/Bin.java
@@ -1,20 +1,26 @@
 package com.github.curriculeon;
 
 public class Bin {
+    private Integer faceValueToTrack;
+    private Integer numberOfOccurrences;
+
+    public Bin(){
+        this.faceValueToTrack = 0;
+        this.numberOfOccurrences = 0;
+    }
+
     public Bin(Integer faceValueToTrack) {
+        this.faceValueToTrack = faceValueToTrack;
     }
 
     Bin(Integer faceValueToTrack, Integer numberOfOccurrences) {
+        this.faceValueToTrack = faceValueToTrack;
+        this.numberOfOccurrences = numberOfOccurrences;
     }
 
-    public Integer getFaceValueToTrack() {
-        return null;
-    }
+    public Integer getFaceValueToTrack() { return this.faceValueToTrack; }
 
-    public Integer getNumberOfOccurrences() {
-        return null;
-    }
+    public Integer getNumberOfOccurrences() { return this.numberOfOccurrences; }
 
-    public void increment() {
-    }
+    public void increment() { this.numberOfOccurrences++; }
 }
